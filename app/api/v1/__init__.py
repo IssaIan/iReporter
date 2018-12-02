66f595479adbe7ee2977361efab51ea67dfc581e
IssaIan <issamwangi@gmail.com>
2018-12-02 14:50:01 +0300

    [ch-routes-#162367494]Updated the endpoints routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
-from app.api.v1.view.views import Incident, Incidents
+from app.api.v1.view.views import Incident, Incidents, Location, Description
 from app.api.v1.view.userviews import User, Users
 
 
@@ -11,3 +11,6 @@ api.add_resource(Incident, '/incidents/<int:incident_id>') #for a specific incid
 api.add_resource(Incidents, '/incidents')  #for the list of incidents
 api.add_resource(User, '/users/<int:user_id>')#for an individual user
 api.add_resource(Users, '/users')#list of all users
+api.add_resource(Location, '/incidents/<int:incident_id>/location')#updating location
+api.add_resource(Description, '/incidents/<int:incident_id>/description')# updating description
+
