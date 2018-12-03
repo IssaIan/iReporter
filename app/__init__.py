2d91f26acf6051e3880967a45c94f277dc5f1af1
IssaIan <issamwangi@gmail.com>
2018-12-04 02:54:36 +0300

    [bg-#162400873]Removed create_tables function call to enable running of travis

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, Blueprint
 from instance.config import app_config
-from db_config import create_tables
 
 from .api.v1 import version_one as v1
 
@@ -9,7 +8,6 @@ def create_app(config_name='testing'):
     app.url_map.strict_slashes = False
     app.config.from_object(app_config['testing'])
     app.config.from_pyfile('config.py')
-    create_tables()
     app.register_blueprint(v1)
     return app
 
