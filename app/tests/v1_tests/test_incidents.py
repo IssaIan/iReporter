1e604e8825dab6fa6af047fcf8a773ef4d117aa3
IssaIan <issamwangi@gmail.com>
2018-12-04 00:42:22 +0300

    [ch-tearDown-#162397371]Merged the two classes in the file and also implemented the use of tearDown method

--- app/tests/v1_tests/test_incidents.py
+++ app/tests/v1_tests/test_incidents.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 from app import create_app
-
+from app.api.v1.model.models import incidents
 
 
 app = create_app('testing')
@@ -40,6 +40,8 @@ class TestIncidents(unittest.TestCase):
 					'videos' : []
 					}
 		
+	def tearDown(self):
+		incidents.clear()
 		
 	def test_create_record(self):
 		response = self.app.post('/api/v1/incidents', 
@@ -50,6 +52,9 @@ class TestIncidents(unittest.TestCase):
 		self.assertEqual(response.status_code, 201)
 	
 	def test_get_incident_byId(self):
+		self.app.post('/api/v1/incidents', 
+										data = json.dumps(self.new_test_incident2),
+										headers={'content-type' : 'application/json'})
 		response = self.app.get('/api/v1/incidents/1')
 		result = json.loads(response.data)
 		self.assertEqual(result['Message'], 'The specific incident has been returned successfully')
@@ -62,48 +67,19 @@ class TestIncidents(unittest.TestCase):
 		self.assertEqual(response.status_code, 404)
 
 	def test_fetch_all_incidents(self):	
+		self.app.post('/api/v1/incidents', 
+										data = json.dumps(self.new_test_incident3),
+										headers={'content-type' : 'application/json'})
 		response = self.app.get('/api/v1/incidents')
 		result = json.loads(response.data)
 		self.assertEqual(result['Message'], 'Incidents returned successfully')
 		self.assertEqual(response.status_code, 200)
 
 
-class TestUpdateIncidents(unittest.TestCase):
-
-	def setUp(self):
-		
-		app.testing = True
-		self.app = app.test_client()
-		self.new_test_incident = {
-					'type' : 'red flag',
-					'title' : 'mine',
-					'status' : 'Under-investigation',
-					'description' : 'Collection of bribes in the KRA building',
-					'location' : 'Nyeri',
-					'images' : [],
-					'videos' : []
-					}
-		self.new_test_incident2 = {
-					'type' : 'intervention',
-					'title' : 'mine',
-					'status' : 'Under-investigation',
-					'description' : 'Collection of bribes in the KRA building',
-					'location' : 'Nyeri',
-					'images' : [],
-					'videos' : []
-					}
-		self.new_test_incident3 = {
-					'type' : 'intervention',
-					'title' : 'yours',
-					'status' : 'draft',
-					'description' : 'Collection of bribes in the KRA building',
-					'location' : 'Nyeri',
-					'images' : [],
-					'videos' : []
-					}
-
 	def test_update_incident_location(self):
-		self.app.post('/api/v1/incidents', json = self.new_test_incident2)
+		self.app.post('/api/v1/incidents', 
+										json = self.new_test_incident2,
+										headers={'content-type' : 'application/json'})
 		response = self.app.patch('/api/v1/incidents/1/location', 
 									json ={ 
 					'type' : 'intervention',
@@ -117,7 +93,9 @@ class TestUpdateIncidents(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_update_incident_description(self):
-		self.app.post('/api/v1/incidents', json = self.new_test_incident3)
+		self.app.post('/api/v1/incidents', 
+										json = self.new_test_incident3,
+										headers={'content-type' : 'application/json'})
 		response = self.app.patch('/api/v1/incidents/1/description', json =  {
 					'type' : 'intervention',
 					'title' : 'yours',
@@ -130,7 +108,9 @@ class TestUpdateIncidents(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_updating_a_nonexistence_incident(self):
-		self.app.post('/api/v1/incidents', json = self.new_test_incident2)
+		self.app.post('/api/v1/incidents', 
+										json = self.new_test_incident2,
+										headers={'content-type' : 'application/json'})
 		response = self.app.patch('/api/v1/incidents/20/description', json =  {
 					'type' : 'intervention',
 					'title' : 'yours',
@@ -142,44 +122,18 @@ class TestUpdateIncidents(unittest.TestCase):
 					})
 		self.assertEqual({'Message' : 'Record not found', 'status': 404 }, response.get_json())
 		
-	
-	
-class TestdeleteIncident(unittest.TestCase):
-	def setUp(self):
-	
-		app.testing = True
-		self.app = app.test_client()
-		self.new_test_incident = {
-					'type' : 'red flag',
-					'title' : 'mine',
-					'status' : 'Under-investigation',
-					'description' : 'Collection of bribes in the KRA building',
-					'images' : [],
-					'videos' : []
-					}
-		self.new_test_incident2 = {
-					'type' : 'intervention',
-					'title' : 'mine',
-					'status' : 'Under-investigation',
-					'description' : 'Collection of bribes in the KRA building',
-					'location' : 'Nyeri',
-					'images' : [],
-					'videos' : []
-					}
-	def test_create_record(self):
-		response = self.app.post('/api/v1/incidents', data = json.dumps(self.new_test_incident2), headers={'content-type' : 'application/json'})
-		result = json.loads(response.data)
-		self.assertEqual(result['Message'], 'Incident saved successfully')
-		self.assertEqual(response.status_code, 201)
-
 	def test_delete_incident(self):
-		self.app.post('/api/v1/incidents', json = self.new_test_incident)
+		self.app.post('/api/v1/incidents', 
+										json = self.new_test_incident,
+										headers={'content-type' : 'application/json'})
 		response = self.app.delete('/api/v1/incidents/1')
 		self.assertEqual({'Message' : 'Successful deletion'}, response.get_json())
 		self.assertEqual(response.status_code, 200)
 
 	def test_fail_delete_incident(self):
-		self.app.post('/api/v1/incidents', json = self.new_test_incident)
+		self.app.post('/api/v1/incidents', 
+										json = self.new_test_incident,
+										headers={'content-type' : 'application/json'})
 		response = self.app.delete('/api/v1/incidents/200')
 		self.assertEqual({'Message' : 'Record not found', 'status': 404 }, response.get_json())
 		
