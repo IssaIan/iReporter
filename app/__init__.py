2cc81b9b5500fca082c3ca9d6fe3e001e8e2b049
IssaIan <issamwangi@gmail.com>
2018-12-03 20:07:56 +0300

    [ch-createappconfiguration-#162387807]Added application configuration names with table/database creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,15 @@
 from flask import Flask, Blueprint
-#from instance.config import app_config
-# from db_config import create_tables
+from instance.config import app_config
+from db_config import create_tables
 
 from .api.v1 import version_one as v1
 
-def create_app(config_name):
-    app = Flask(__name__)
+def create_app(config_name='testing'):
+    app = Flask(__name__, instance_relative_config=True)
+    app.url_map.strict_slashes = False
+    app.config.from_object(app_config['testing'])
+    app.config.from_pyfile('config.py')
+    create_tables()
     app.register_blueprint(v1)
     return app
 
-
-# def create_app():#config_name='development'):
-#     app = Flask(__name__)#(__name__, instance_relative_config=True)
-#     #app.url_map.strict_slashes = False
-#     #app.config.from_object(app_config[config_name])
-#     #app.config.from_pyfile('config.py')
-#     # create_tables()
-#     app.register_blueprint(v1)
-#     return app
-  
