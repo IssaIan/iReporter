b1b34c858e01c6229c56debebe4561b256ac80ea
IssaIan <issamwangi@gmail.com>
2018-12-04 13:34:36 +0300

    [ft-addroutes-#162410113]Added an endpoint route to access users via their username

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
 from app.api.v1.view.views import Incident, Incidents, Location, Description
-from app.api.v1.view.userviews import User, Users
+from app.api.v1.view.userviews import User, Users, UserbyUsername
 
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
@@ -13,4 +13,4 @@ api.add_resource(User, '/users/<int:user_id>')#for an individual user
 api.add_resource(Users, '/users')#list of all users
 api.add_resource(Location, '/incidents/<int:incident_id>/location')#updating location
 api.add_resource(Description, '/incidents/<int:incident_id>/description')# updating description
-
+api.add_resource(UserbyUsername, '/users/<user_name>')#gets a user by their username and also enables patching
