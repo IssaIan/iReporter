7a36617e38e121393153a3f067764da45d327370
IssaIan <issamwangi@gmail.com>
2018-12-08 21:55:09 +0300

    Formatted according to pep8 standards

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,14 +3,17 @@ from flask import Blueprint
 from app.api.v1.view.views import Incident, Incidents, Location, Description
 from app.api.v1.view.userviews import User, Users, UserbyUsername
 
-
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
-api.add_resource(Incident, '/incidents/<int:incident_id>') #for a specific incident
-api.add_resource(Incidents, '/incidents')  #for the list of incidents
-api.add_resource(User, '/users/<int:user_id>')#for an individual user
-api.add_resource(Users, '/users')#list of all users
-api.add_resource(Location, '/incidents/<int:incident_id>/location')#updating location
-api.add_resource(Description, '/incidents/<int:incident_id>/description')# updating description
-api.add_resource(UserbyUsername, '/users/<user_name>')#gets a user by their username and also enables patching
+# for a specific incident
+api.add_resource(Incident, '/incidents/<int:incident_id>')
+api.add_resource(Incidents, '/incidents')  # for the list of incidents
+api.add_resource(User, '/users/<int:user_id>')  # for an individual user
+api.add_resource(Users, '/users')  # list of all users
+# updating location
+api.add_resource(Location, '/incidents/<int:incident_id>/location')
+# updating description
+api.add_resource(Description, '/incidents/<int:incident_id>/description')
+# gets a user by their username and also enables patching
+api.add_resource(UserbyUsername, '/users/<user_name>')
