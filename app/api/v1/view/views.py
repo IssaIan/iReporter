0c49d1c7c591de729730da76f1ed3818b0c5b602
IssaIan <issamwangi@gmail.com>
2018-12-08 22:05:08 +0300

    [ch-validations-#162527953]Added isspace() function for validation

--- app/api/v1/view/views.py
+++ app/api/v1/view/views.py
@@ -2,109 +2,108 @@ from flask_restful import Resource, reqparse
 from flask import request
 from app.api.v1.model.models import Models
 
+
 incidents = []
 
 parser = reqparse.RequestParser()
 parser.add_argument(
-    "type", type= str, required = True, help= "Type field is required")
+    "type", type=str, required=True, help="Type field is required")
 parser.add_argument(
-    "title", type= str, required = True, help= "Title field is required")
+    "title", type=str, required=True, help="Title field is required")
 parser.add_argument(
-    "status", type=str, default= "Draft"
+    "status", type=str, default="Draft"
 )
 parser.add_argument(
-    "description", type= str, required = True, help= "Description field is required"
-    )
+    "description", type=str, required=True, help="Description field is required"
+)
 parser.add_argument(
-    "location", type= str, required = True, help= "Location field is required")
+    "location", type=str, required=True, help="Location field is required")
 parser.add_argument(
-    "images", type= str, required = False)
+    "images", type=str, required=False)
 parser.add_argument(
-    "videos", type= str, required = False)
+    "videos", type=str, required=False)
 
 
 class GetError():
-    #declare a function notFound() to enble inheritance of the function by other classes
+    # declare a function notFound() to enble inheritance of the function by other classes
 
     def notFound(self):
-        return {'Message' : 'Record not found','status':404},404
+        return {'Message': 'Record not found', 'status': 404}, 404
+
 
 class Incidentbase(Models):
-    #This class initiates a model for storing incidents
+    # This class initiates a model for storing incidents
     def __init__(self):
-        # Initializes a the Model class with storage reference of incidents 
+        # Initializes a the Model class with storage reference of incidents
         super().__init__(incidents)
 
 
-    
-
 class Incident(Resource, GetError):
-    #This class and its members creates an endpoint where only a single incident can be acted upon
+    # This class and its members creates an 
+    # endpoint where only a single incident can be acted upon
 
     def __init__(self):
         self.db = Incidentbase()
 
     def get(self, incident_id):
-        incident = self.db.find(incident_id) 
+        incident = self.db.find(incident_id)
         if not incident:
-            return self.notFound()    
+            return self.notFound()
         else:
-            return {'Message' : 'The specific incident has been returned successfully',
-                    'data' : incident
+            return {'Message': 'The specific incident has been returned successfully',
+                    'data': incident
                     }, 200
-            
+
     def delete(self, incident_id):
         incident = self.db.erase(incident_id)
         if not incident:
-            return self.notFound()     
+            return self.notFound()
         else:
-            return {'Message' : 'Successful deletion'}, 200           
+            return {'Message': 'Successful deletion'}, 200
 
 
 class Incidents(Resource, GetError):
-    #This class and its members creates an endpoint where only several incidents can be acted upon
+    # This class and its members creates an endpoint 
+    # where only several incidents can be acted upon
 
     def __init__(self):
         self.db = Incidentbase()
-   
+
     def get(self):
         if self.db.all() == []:
-            return self.notFound() 
+            return self.notFound()
         return {
-                'Message' : 'Incidents returned successfully',
-                'data' : self.db.all()
-                }, 200
+            'Message': 'Incidents returned successfully',
+            'data': self.db.all()
+        }, 200
 
     def post(self):
-        
+
         data = parser.parse_args()
 
         incident = {
             'type': data['type'],
-            'title' : data['title'],
+            'title': data['title'],
             'status': data['status'],
-            'description' : data['description'],
-            'images' : [],
-            'videos' : []
-            }
-        if data['type'] == "" or data['type'] == " ":
-            return "Type of incident required!"
-        elif data['title'] == "" or data['title'] == " ":
-            return "Title of incident required!"
-        elif data['description'] == "" or data['description'] == " ":
-            return "Description of incident required!"
-        else:
-            self.db.save(incident)
-            return {'Message' : 'Incident saved successfully',
-                    'data' : incident
+            'description': data['description'],
+            'images': [],
+            'videos': []
+        }
+        if data['type'].isspace() or data['title'].isspace() or data['description'].isspace():
+            return {'Message': 'Field cannot be empty'}, 400
+        self.db.save(incident)
+        return {'Message': 'Incident saved successfully',
+                'data': incident
                 }, 201
 
+
 class Location(Resource, GetError):
-    #This class and its members creates an endpoint where only a single incident's location can be updated 
+    # This class and its members creates an endpoint where 
+    # only a single incident's location can be updated
 
     def __init__(self):
         self.db = Incidentbase()
-    
+
     def patch(self, incident_id):
 
         data = parser.parse_args()
@@ -113,20 +112,20 @@ class Location(Resource, GetError):
         if incident:
             for incident['location'] in incident:
                 incident.update(data)
-                return {'Message' : 'Successfully updated',
-                    'data' : incident
-                    }, 200
+                return {'Message': 'Successfully updated',
+                        'data': incident
+                        }, 200
         else:
             return self.notFound()
-       
 
 
 class Description(Resource, GetError):
-    #This class and its members creates an endpoint where only a single incident's descriptionp can be updated 
+    # This class and its members creates an endpoint 
+    # where only a single incident's descriptionp can be updated
 
     def __init__(self):
         self.db = Incidentbase()
-   
+
     def patch(self, incident_id):
 
         data = parser.parse_args()
@@ -135,8 +134,9 @@ class Description(Resource, GetError):
         if incident:
             for incident['description'] in incident:
                 incident.update(data)
-                return {'Message' : 'Successfully updated',
-                    'data' : incident
-                    }, 200
+                return {'Message': 'Successfully updated',
+                        'data': incident
+                        }, 200
         else:
             return self.notFound()
+
